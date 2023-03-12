58648b3e7931825965bc8e727ccb360289e32b3d
Cgotts <cobi.gottschalk@gmail.com>
2023-03-12 11:27:10 -0700

    Removed unnecessary files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', {
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/books', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,20 +5,17 @@ const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('savedBooks');
-    },
+    // user: async (parent, { username }) => {
+    //   return User.findOne({ username }).populate('savedBooks');
+    // },
+    // user: async (parent, { userId }) => {
+    //   return User.findOne({ _id: userId });
+    // },
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
-          .populate('savedBooks');
-
-        return userData;
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
       }
-
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 
